72cefddd85f8e0a110bda44f9b5c0bc6fa5696ee
unknown <natalia_m_luz@saojose.sc.senai.br>
2019-06-17 08:48:33 -0300

    modulo 3 aula 2

---
+++ mod3/todo.js
@@ -0,0 +1,7 @@
+var listElement = document.querySelector("#app ul");
+var listInput = document.querySelector("#app input");
+var listButton = document.querySelector("#app button");
+
+console.log(listElement);
+console.log(listInput);
+console.log(listButton);
